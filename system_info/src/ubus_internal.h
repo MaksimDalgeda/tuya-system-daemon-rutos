1a6c5f72d65f7fb74976fdd143727d2cd4a41d0e
Maksim Dalgeda <your.email@example.com>
2026-09-08 15:04:04 +0300

    bug fix

--- system_info/src/ubus_internal.h
+++ system_info/src/ubus_internal.h
@@ -3,8 +3,8 @@
 
 #include "ubus.h"
 
-Error_Code ubus_get_system_info(); //uptime, memory 
+Error_Code ubus_get_system_info(system_info_t *info); //uptime, memory 
 Error_Code ubus_get_system_network(); //name, ip , netmask
-Error_Code ubus_get_rx_tx_bytes(); //trafic data
+Error_Code ubus_get_traffic(); //trafic data
 
 #endif
