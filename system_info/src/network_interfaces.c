036b1c0f5774630a72317cf71c2251859b5579c1
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-14 16:33:31 +0300

    interface names collector using UCI

---
+++ system_info/src/network_interfaces.c
@@ -0,0 +1,46 @@
+#include <uci.h>
+
+#include "ubus_internal.h"
+
+Error_Code get_network_interfaces(network_info_t *network, size_t *network_count)
+{
+    struct uci_context *ctx;
+    struct uci_package *pkg;
+    struct uci_element *e;
+
+    if (!network || !network_count)
+        return ERROR;
+
+    *network_count = 0;
+
+    ctx = uci_alloc_context();
+    if (!ctx)
+        return ERROR;
+
+    if (uci_load(ctx, "network", &pkg) != UCI_OK) {
+        uci_free_context(ctx);
+        return ERROR;
+    }
+
+    uci_foreach_element(&pkg->sections, e) {
+
+        struct uci_section *s = uci_to_section(e);
+
+        if (strcmp(s->type, "interface") != 0)
+            continue;
+
+        if (strcmp(s->e.name, "loopback") == 0)
+            continue;
+
+        strncpy(network[*network_count].name, s->e.name, sizeof(network[*network_count].name) - 1);
+
+        network[*network_count].name[sizeof(network[*network_count].name) - 1] = '\0';
+
+        (*network_count)++;
+    }
+
+    uci_unload(ctx, pkg);
+    uci_free_context(ctx);
+
+    return OK;
+}
--- system_info/src/ubus_internal.h
+++ system_info/src/ubus_internal.h
@@ -4,6 +4,7 @@
 #include "ubus.h"
 
 Error_Code ubus_get_system_info(system_info_t *info);
+Error_Code get_network_interfaces(network_info_t *network, size_t *network_count);
 Error_Code ubus_get_system_network(network_info_t *network, size_t *network_count);
 Error_Code ubus_get_system_traffic(network_info_t *network, size_t *network_count);
 
